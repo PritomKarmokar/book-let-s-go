e57df485c28b47d9a7126f2cc4c14b546e153cc1
Pritom Karmokar <pritomkarmokar99@gmail.com>
2024-10-26 02:35:07 +0600

    adding embed

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io/fs"
 	"path/filepath"
+	"snippetbox.alexedwards.net/ui"
 	"text/template"
 	"time"
 
@@ -29,7 +31,7 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
@@ -37,20 +39,16 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
 		cache[name] = ts
 	}
 	return cache, nil
